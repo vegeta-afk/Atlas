17aa3bd9266298281fca952c5f7d5ac717a1921f
Anidh Kaushik <onlinebusiness1504@gmail.com>
2026-08-18 17:04:45 +0530

    fix:yea

--- client/src/pages/students/MaterialIssue.jsx
+++ client/src/pages/students/MaterialIssue.jsx
@@ -423,9 +423,9 @@ const MaterialIssue = () => {
                     <table className="data-table">
                       <thead>
                                                 <tr>
-                          <th>Student ID</th>
-                          <th>Date of Admission</th>
-                          <th style={{ textAlign: "left" }}>Student Name</th>
+                                                    <th>Student ID</th>
+                          <th style={{ textAlign: "center" }}>Date of Admission</th>
+                          <th style={{ textAlign: "center" }}>Student Name</th>
                           <th>Contact</th>
                           <th>Batch</th>
                           <th>Materials Issued</th>
@@ -445,7 +445,7 @@ const MaterialIssue = () => {
                             return (
                                                             <tr key={student._id}>
                                 <td className="student-id">{student.studentId}</td>
-                                                                <td style={{ textAlign: "center" }}>{formatDate(student.admissionDate)}</td>
+                                 <td style={{ textAlign: "center" }}>{formatDate(student.admissionDate)}</td>
                                 <td>
                                   <div className="student-info">
                                     <div className="avatar">{student.fullName ? student.fullName.charAt(0) : "?"}</div>
