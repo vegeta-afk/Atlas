1d238f1755ca431ca6dfcf3247db4bc149a7dfa8
Anidh Kaushik <onlinebusiness1504@gmail.com>
2026-08-22 12:32:03 +0530

    fix:added patch

--- server/src/app.js
+++ server/src/app.js
@@ -20,7 +20,7 @@ app.use(
   cors({
     origin: ["http://localhost:5173", "https://atlas-green-two.vercel.app"], // Your React app URL
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS" , "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization", "Accept"],
   })
 );
